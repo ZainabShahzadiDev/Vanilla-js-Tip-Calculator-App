a44f6eba4389e51b2cb5c134dd862990891e47ef
ZainabShahzadiDev <zainabshahzadi.contact@gmail.com>
2026-07-23 17:06:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,81 @@
-const answers = document.querySelectorAll(".accordian-answer");
-const images = document.querySelectorAll(".accordian-question img");
+let billInput = document.querySelector(".billInput");
+let reset = document.querySelector(".reset");
+let selectButtons = document.querySelectorAll(".buttons button");
+let numberOfPeopleInput = document.querySelector(".numberOfPeopleInput");
+let peopleBox = document.querySelector(".numberOfPeople .input-box")
+let tipAmount = document.querySelector(".tipAmount");
+let totalAmount = document.querySelector(".totalAmount");
+let error = document.querySelector(".error");
+let customBtn = document.querySelector(".customBtn");
 
-images.forEach((image, index) => {
 
-    image.addEventListener("click", () => {
 
-        answers[index].classList.toggle("show");
+selectButtons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
 
-        if (answers[index].classList.contains("show")) {
-            image.src = "assets/images/icon-minus.svg";
-        } else {
-            image.src = "assets/images/icon-plus.svg";
+        if (billInput.value === "" || numberOfPeopleInput.value === "") {
+            tipAmount.textContent = "$0.00";
+            totalAmount.textContent = "$0.00";
+            return;
         }
 
+        let bill = Number(billInput.value);
+        let people = Number(numberOfPeopleInput.value);
+
+
+        if (numberOfPeopleInput.value !== "" && people === 0) {
+            tipAmount.textContent = "$0.00";
+            totalAmount.textContent = "$0.00";
+            error.classList.add("show");
+            peopleBox.classList.add("border-show");
+            return;
+        }
+
+        error.classList.remove("show");
+        peopleBox.classList.remove("border-show");
+
+        let tipPercentage = btn.textContent;
+        let tipPercentageNumber = Number(tipPercentage.replace("%", ""));
+
+        let tip = (bill * tipPercentageNumber) / 100;
+        tipAmount.textContent = `$${tip.toFixed(2)}`;
+        let totalTip = bill + tip;
+        let totalPerPerson = totalTip / people;
+        totalAmount.textContent = `$${totalPerPerson.toFixed(2)}`;
     });
+})
+
+numberOfPeopleInput.addEventListener("input", () => {
+
+    if (Number(numberOfPeopleInput.value) === 0) {
+        error.classList.add("show");
+        peopleBox.classList.add("border-show");
+
+        tipAmount.textContent = "$0.00";
+        totalAmount.textContent = "$0.00";
+    } else {
+        error.classList.remove("show");
+        peopleBox.classList.remove("border-show");
+    }
 
 });
+
+
+// customBtn.addEventListener("click", () => {
+//     customBtn.textContent = 
+// })
+
+
+reset.addEventListener("click", (e) => {
+    e.preventDefault();
+    billInput.value = "";
+    numberOfPeopleInput.value = "";
+
+    tipAmount.textContent = "$0.00";
+    totalAmount.textContent = "$0.00";
+
+    peopleBox.classList.remove("border-show");
+    error.classList.remove("show");
+})
+
