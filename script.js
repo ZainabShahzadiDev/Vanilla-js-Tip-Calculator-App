d6886c674b250b2bca22ca39a38bf7fcfd960363
ZainabShahzadiDev <zainabshahzadi.contact@gmail.com>
2026-07-23 16:35:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,18 @@
-let billInput = document.querySelector(".billInput");
-let reset = document.querySelector(".reset");
-let selectButtons = document.querySelectorAll(".buttons button");
-let numberOfPeopleInput = document.querySelector(".numberOfPeopleInput");
-let peopleBox = document.querySelector(".numberOfPeople .input-box")
-let tipAmount = document.querySelector(".tipAmount");
-let totalAmount = document.querySelector(".totalAmount");
-let customBtn = document.querySelector(".customBtn");
+const answers = document.querySelectorAll(".accordian-answer");
+const images = document.querySelectorAll(".accordian-question img");
 
+images.forEach((image, index) => {
 
+    image.addEventListener("click", () => {
 
-selectButtons.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
+        answers[index].classList.toggle("show");
 
-        if (billInput.value === "" || numberOfPeopleInput.value === "") {
-            return;
+        if (answers[index].classList.contains("show")) {
+            image.src = "assets/images/icon-minus.svg";
+        } else {
+            image.src = "assets/images/icon-plus.svg";
         }
 
-        let bill = Number(billInput.value);
-        let people = Number(numberOfPeopleInput.value);
-
-
-        let tipPercentage = btn.textContent;
-        let tipPercentageNumber = Number(tipPercentage.replace("%", ""));
-
-        if (people === 0) {
-            peopleBox.style.border = "2px solid orange";
-            return;
-        }
-
-        peopleBox.style.border = "";
-
-        let tip = (bill * tipPercentageNumber) / 100;
-        tipAmount.textContent = `$${tip.toFixed(2)}`;
-        let totalTip = bill + tip;
-        let totalPerPerson = totalTip / people;
-        totalAmount.textContent = `$${totalPerPerson.toFixed(2)}`;
     });
 
-
-
-})
-
-// customBtn.addEventListener("click", () => {
-//     customBtn.textContent = 
-// })
-
-
-// function numberOfPeopleInputBox() {
-// }
-
-reset.addEventListener("click", (e) => {
-    e.preventDefault();
-    billInput.value = "";
-    numberOfPeopleInput.value = "";
-
-    tipAmount.textContent = "$0.00";
-    totalAmount.textContent = "$0.00";
-
-    peopleBox.style.border = "";
-})
-
+});
